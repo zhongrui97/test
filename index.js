75f20b458d876104da2e90b3d7f7b5ba759a0b5c
zorro <zhongrui97@gmail.com>
2020-10-18 16:47:38 +0800

    2

--- index.js
+++ index.js
@@ -2,20 +2,17 @@ const Koa = require('koa');
 const app = new Koa();
 
 app.use(async (ctx, next) => {
-  console.log('A-start');
+  console.log('AA-start');
   await next();
-  console.log('A-end');
+  console.log('AA-end');
 });
 
 app.use(async (ctx, next) => {
-  console.log('B-start')
+  console.log('BB-start')
   await next();
-  console.log('B-end');
+  console.log('BB-end');
 
 });
-
-
-
 
 app.use(async ctx => {
   ctx.body = 'Hello World';
